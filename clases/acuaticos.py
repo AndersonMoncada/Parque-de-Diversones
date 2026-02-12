962caa36f605989b41457b2757683a68d5158e04
Anderson M <andersonmoncada1128797@correo.itm.edu.co>
2026-02-12 18:53:42 -0500

    feat: agregar cambios

--- clases/acuaticos.py
+++ clases/acuaticos.py
@@ -1,14 +1,16 @@
 class Acuatico:
-    def __init__(self, capacidad: int, profundidad: int, propulsion: str):
+    def __init__(self, capacidad: int, profundidad: float, propulsion: str):
         self.capacidad = capacidad
         self.profundidad = profundidad
         self.propulsion = propulsion
 
     def imprimir_data(self):
         print(
-            f"La capacidad de la atraccion acuatica es {self.capacidad}, su profundidad es {self.profundidad}, y su tipo de propulsion es : {self.propulsion}"
+            f"La capacidad de la atraccion acuatica es {self.capacidad} personas, su profundidad es {self.profundidad} metros, y su tipo de propulsion es : {self.propulsion}"
         )
 
 
-Tobogan = Acuatico("20 Personas", "20 metros", " Tobogan")
+Tobogan = Acuatico(20, 1.70, " Tobogan")
 Tobogan.imprimir_data()
+
+Piscina = Acuatico(50, 1.90, "Pasiva")
