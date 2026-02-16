f08ef9f86d35b2234ed5f951abb2db143a180fdf
danikpop88 <danielarestrepocanu@gmail.com>
2026-02-16 15:29:19 -0500

    Cambio realizado y nuevo metodo añadido

--- clases/fisicos.py
+++ clases/fisicos.py
@@ -4,9 +4,22 @@ class JuegosFisicos:
         self.altura_juego=altura_juego
         self.edad_minima=edad_minima
         self.estatura_minima=estatura_minima
-    def mostrarInfo (self):
-        print("Nombre:",self.nombre_juego,"\n Altura del juego:",self.altura_juego,"\n Edad minima:",self.edad_minima,"\n Estatura minima:",self.estatura_minima)
 
-tiroleza=JuegosFisicos("tiroleza",29.3,14,1.60)    
-tiroleza.mostrarInfo()
+    def mostrarInfo (self) -> str:
+        print("Nombre:",self.nombre_juego,"\n Altura del juego:",self.altura_juego,"\n Edad minima:",self.edad_minima,"\n Estatura minima:",self.estatura_minima)
+    
+    def validar_condiciones_entrada(self, edad: int, estatura: float) -> str:
+        if edad >= self.edad_minima and estatura >= self.estatura_minima:
+            return "Puedes entrar al juego"
+        else:
+            return "No puedes entrar al juego, lo siento"
+
+
+tirolesa=JuegosFisicos("Tirolesa",29.3,14,1.60)
+muro_escalar=JuegosFisicos("Muro para escalar",30, 12, 1.50)
+
+muro_escalar.mostrarInfo()
+mensaje=muro_escalar.validar_condiciones_entrada(15,1.40)
+
+    
 
